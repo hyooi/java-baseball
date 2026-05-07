222b8edf14ba51dfe52769990c7af0875392a1a4
hyoin <hyoin01@gmail.com>
2026-05-07 14:42:50 +0900

    feat: add DslTest for SkillBuilder

--- src/test/java/study/DslTest.kt
+++ src/test/java/study/DslTest.kt
@@ -26,9 +26,11 @@ class DslTest {
             introduce {
                 name("강효인")
                 company("현대자동차그룹")
-//                skills {
-//                    soft("A passion for problem solving")
-//                }
+                skills {
+                    soft("A passion for problem solving")
+                    soft("Good communication skills")
+                    hard("Kotlin")
+                }
                 languages {
                     "Korean" level 5
                     "English" level 3
@@ -37,6 +39,12 @@ class DslTest {
 
         actual.name shouldBe "강효인"
         actual.company shouldBe "현대자동차그룹"
+        actual.skill[0].type shouldBe SkillType.SOFT
+        actual.skill[0].description shouldBe "A passion for problem solving"
+        actual.skill[1].type shouldBe SkillType.SOFT
+        actual.skill[1].description shouldBe "Good communication skills"
+        actual.skill[2].type shouldBe SkillType.HARD
+        actual.skill[2].description shouldBe "Kotlin"
         actual.languages[0].language shouldBe "Korean"
         actual.languages[0].level shouldBe 5
         actual.languages[1].language shouldBe "English"
@@ -56,9 +64,15 @@ class PersonBuilder {
     private lateinit var name: String // 지연초기화
     private var company: String = ""
     private var languages: List<Language> = emptyList()
+    private var skills: List<Skill> = emptyList()
 
     fun name(name: String) { this.name = name }
     fun company(company: String) { this.company = company }
+    fun skills(block: SkillBuilder.() -> Unit) {
+        skills = SkillBuilder()
+            .apply(block)
+            .build()
+    }
     fun languages(block: LanguageBuilder.() -> Unit) {
         languages = LanguageBuilder()
             .apply(block)
@@ -67,7 +81,19 @@ class PersonBuilder {
 
 //    fun build(): Person = Person(name!!) //null회피1 : name이 null이 아니라고 강제로 알려줌. 만약 null이면 NPE
 //    fun build(): Person = Person(name ?: "default name") //null회피2
-    fun build(): Person = Person(name, company, languages)
+    fun build(): Person = Person(name, company, skills, languages)
+}
+
+class SkillBuilder {
+    private val skills = mutableListOf<Skill>()
+    fun soft(description: String) {
+        skills.add(Skill(SkillType.SOFT, description))
+    }
+    fun hard(description: String) {
+        skills.add(Skill(SkillType.HARD, description))
+    }
+    fun build(): List<Skill> = skills
+
 }
 
 class LanguageBuilder {
@@ -82,9 +108,15 @@ class LanguageBuilder {
 
 class Person(val name: String,
              val company: String,
+             val skill: List<Skill>,
              val languages: List<Language>) {}
 
-class Skill {}
+data class Skill(val type: SkillType, val description: String) {}
+
+enum class SkillType {
+    SOFT, HARD
+}
+
 data class Language(val language: String,
     val level: Int) {
 }
